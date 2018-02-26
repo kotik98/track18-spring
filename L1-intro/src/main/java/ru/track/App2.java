682e718c66b3f580f806d4c6d3a865a51b17af69
Bogdan <zaharin.98@mail.ru>
2018-02-26 13:57:07 +0300

    task 1: intro

--- L1-intro/src/main/java/ru/track/App2.java
+++ L1-intro/src/main/java/ru/track/App2.java
@@ -3,9 +3,12 @@ package ru.track;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Arrays;
+
 public class App2 {
 
     public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
         String str = StringUtils.capitalize(args[0]);
         System.out.println(str);
     }
