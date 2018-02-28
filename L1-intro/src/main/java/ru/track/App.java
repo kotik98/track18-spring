57b5c7bda955c7cc22c581412817a2d35ad0f773
Bogdan <zaharin.98@mail.ru>
2018-02-28 18:17:40 +0300

    task 1: intro

--- L1-intro/src/main/java/ru/track/App.java
+++ L1-intro/src/main/java/ru/track/App.java
@@ -1,20 +1,11 @@
 package ru.track;
 
-/**
- * TASK:
- * POST request to  https://guarded-mesa-31536.herokuapp.com/track
- * fields: name,github,email
- *
- * LIB: http://unirest.io/java.html
- *
- *
- */
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import org.json.JSONObject;
 
-public class App {
+public class App2 {
 
     public static final String URL = "http://guarded-mesa-31536.herokuapp.com/track";
     public static final String FIELD_NAME = "name";
