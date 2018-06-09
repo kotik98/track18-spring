69fc111aa05bb82d08f74a2d60fb491811b6dbc1
kotik98 <zaharin.98@mail.ru>
2018-06-09 20:35:26 +0300

    Update App2.java

--- L1-intro/src/main/java/ru/track/App2.java
+++ L1-intro/src/main/java/ru/track/App2.java
@@ -8,7 +8,6 @@ import java.util.Arrays;
 public class App2 {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(args));
         String str = StringUtils.capitalize(args[0]);
         System.out.println(str);
     }
