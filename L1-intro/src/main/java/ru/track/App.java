5578a9577a0557008112b8b39bb6bd4cb4958018
kotik98 <zaharin.98@mail.ru>
2018-06-09 20:50:59 +0300

    Update App.java

--- L1-intro/src/main/java/ru/track/App.java
+++ L1-intro/src/main/java/ru/track/App.java
@@ -9,11 +9,6 @@ package ru.track;
  *
  *
  */
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
-import com.mashape.unirest.http.Unirest;
-import org.json.JSONObject;
-
 public class App {
 
     public static final String URL = "http://guarded-mesa-31536.herokuapp.com/track";
@@ -24,16 +19,13 @@ public class App {
     public static void main(String[] args) throws Exception {
         // 1) Use Unirest.post()
         // 2) Get response .asJson()
-        HttpResponse<JsonNode> response = Unirest.post(URL)
-                .field(FIELD_NAME, "Bogdan")
-                .field(FIELD_GITHUB, "kotik98")
-                .field(FIELD_EMAIL, "zaharin.98@mail.ru")
-                .field("success", false)
-                .asJson();
         // 3) Get json body and JsonObject
-        JSONObject jsonObject = response.getBody().getObject();
         // 4) Get field "success" from JsonObject
-        boolean success = jsonObject.getBoolean("success");
-        System.out.println(success);
+        
+        
+        
+        
+        boolean success = false;
     }
+    
 }
